0917198a236eb9ea1c9ee1bac7f57269abbbba1a
RJ Mey <rj@rjmey.com>
2017-01-23 17:40:43 -0500

    Initial commit

---
+++ ch2/embed.php
@@ -0,0 +1,34 @@
+<?php
+$book = [
+    'title' => "The Hitchhiker's Guide to the Galaxy",
+    'author' => 'Douglas Adams',
+    'description' => 'a comedy sci-fi adventure originally based on a BBC radio series'
+];
+$characters = [
+    'Arthur Dent',
+    'Ford Prefect',
+    'Zaphod Beeblebrox',
+    'Marvin, the paranoid android',
+    'Slartibartfast'
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Challenge: Embed in HTML</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Book <?php echo $book['title']; ?>, </h1> by <?php echo $book['author'] ?>
+<p>Description: <?php echo $book['description'] ?></p>
+<h2>Main Characters</h2>
+<ul>
+<?php
+    foreach ($characters as $character){
+        echo "<li>$character</li>";
+    }
+?>
+</ul>
+</body>
+</html>
